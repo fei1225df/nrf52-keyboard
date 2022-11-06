9680a728509b1524319f9487b05233e23710be4a
fei1225df <104438705+fei1225df@users.noreply.github.com>
2022-11-06 15:54:37 +0800

    Update actionmap_common.h

--- keyboard/gt-planck/b/actionmap_common.h
+++ keyboard/gt-planck/b/actionmap_common.h
@@ -34,10 +34,10 @@ extern const action_t actionmaps[][MATRIX_ROWS][MATRIX_COLS];
  * 第二组为按键实际矩阵
  */
 #define ACTIONMAP_ENC( \
-	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
-	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
-	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
-	K30, K31, K32, K33, K34, K35,      K37, K38, K39, K3A, K3B, \
+	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, \
+	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, \
+	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, \
+	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, \
 	R0P, R0N, K4C \
 ) { \
 	{ AC_##K00, AC_##K01, AC_##K02, AC_##K03, AC_##K04, AC_##K05, AC_##K06, AC_##K07, AC_##K08, AC_##K09, AC_##K0A, AC_##K0B, AC_NO    }, \
