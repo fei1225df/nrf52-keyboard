d14658093a3a7b4c8530c36f43eb40c9f2e4213f
fei1225df <104438705+fei1225df@users.noreply.github.com>
2022-11-06 13:31:42 +0800

    Update actionmap_common.h

--- keyboard/gt-planck/b/actionmap_common.h
+++ keyboard/gt-planck/b/actionmap_common.h
@@ -43,6 +43,6 @@ extern const action_t actionmaps[][MATRIX_ROWS][MATRIX_COLS];
 	{ AC_##K00, AC_##K01, AC_##K02, AC_##K03, AC_##K04, AC_##K05, AC_##K06, AC_##K07, AC_##K08, AC_##K09, AC_##K0A, AC_##K0B, AC_##K0C, AC_##K0D, AC_##K0E, AC_##K0F   }, \
 	{ AC_##K10, AC_##K11, AC_##K12, AC_##K13, AC_##K14, AC_##K15, AC_##K16, AC_##K17, AC_##K18, AC_##K19, AC_##K1A, AC_##K1B, AC_##K1C, AC_##K1D, AC_NO,    AC_NO    }, \
 	{ AC_##K20, AC_##K21, AC_##K22, AC_##K23, AC_##K24, AC_##K25, AC_##K26, AC_##K27, AC_##K28, AC_##K29, AC_##K2A, AC_##K2B, AC_##K2C, AC_NO,    AC_NO,    AC_NO   }, \
-	{ AC_##K30, AC_##K31, AC_##K32, AC_##K33, AC_##K34, AC_##K35, AC_NO   , AC_##K37, AC_##K38, AC_##K39, AC_##K3A, AC_##K3B, AC_##K3C, AC_NO,    AC_NO,    AC_NO   }, \
+	{ AC_##K30, AC_##K31, AC_##K32, AC_##K33, AC_##K34, AC_##K35, AC_##K36, AC_##K37, AC_##K38, AC_##K39, AC_##K3A, AC_##K3B, AC_##K3C, AC_NO,    AC_NO,    AC_NO   }, \
 	{ AC_##K40, AC_##K41, AC_##K42, AC_NO   , AC_NO   , AC_##K45, AC_NO   , AC_NO   , AC_##K48, AC_##K49, AC_##K4A, AC_##K4B, AC_##K4C, AC_NO,    AC_##K4E, AC_##K4F }, \
 }
