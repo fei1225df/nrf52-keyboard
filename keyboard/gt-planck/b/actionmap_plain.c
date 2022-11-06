afe180957c2d301b036e02af6113d538e8feda2d
fei1225df <104438705+fei1225df@users.noreply.github.com>
2022-11-06 15:54:35 +0800

    Update actionmap_plain.c

--- keyboard/gt-planck/b/actionmap_plain.c
+++ keyboard/gt-planck/b/actionmap_plain.c
@@ -36,17 +36,17 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 const action_t actionmaps[][MATRIX_ROWS][MATRIX_COLS] = { 
 	[0] = ACTIONMAP_ENC(
-	ESC,    1,    2,    3,    4,    5,    6,    7,    8,    9,    0,     BSPC,
-	TAB,    A,    S,    D,    F,    G,    H,    J,    K,    L,    SCLN,  ENTER,
-	LSFT,   Z,    X,    C,    V,    B,    N,    M,    COMM, DOT,  SLSH,  RSFT,
-	LCTL,  LGUI, LALT, CAPS,  FN0,   SPACE,     FN0,  DEL,  RALT, APP,   RCTL,
-	VOLU,  VOLD,  ESC ),
+	ESC,    1,    2,    3,    4,    5,    6,    7,    8,    9,    0,     MINS,     BSPC,
+	TAB,    Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,     LBRC,     RBRC,
+	CAPS,   A,    S,    D,    F,    G,    H,    J,    K,    L,    SCLN,  DUOT,     ENTER,
+	LSFT,   Z,    X,    C,    V,    B,    N,    M,    COMM,  DOT, SLSH,  RSFT,     UP,
+	LCTL,   LGUI, LALT ),
 
 	[1] = ACTIONMAP_ENC(
-	ESC,    1,    2,    3,    4,    5,    6,    7,    8,    9,    0,     BSPC,
-	F1,     F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,   F12,
-	LSFT,   Z,    X,    C,    V,    B,    N,    M,    COMM, DOT,  SLSH,  RSFT,
-	LCTL,  LGUI, LALT, CAPS,  FN0,   SPACE,     FN0,  DEL,  RALT, APP,   RCTL,
+	ESC,    1,    2,    3,    4,    5,    6,    7,    8,    9,    0,     MINS,     BSPC,
+	F1,     F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,   F12,      RBRC,
+	CAPS,   A,    S,    D,    F,    G,    H,    J,    K,    L,    SCLN,  DUOT,     ENTER,
+	LSFT,   Z,    X,    C,    V,    B,    N,    M,    COMM,  DOT, SLSH,  RSFT,     UP,
 	VOLU,  VOLD,  ESC ),
 };
 
